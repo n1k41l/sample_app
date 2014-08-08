1552c396f6b90256a226f0fc8a3b7497c79f9d77
nikhil patel <nikhil.n.patel@gmail.com>
2014-08-08 17:11:42 +0100

    started user password tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ActiveRecord::Base
 	validates( :email, presence: true )
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validate( :email, presence: true, format: { with: VALID_EMAIL_REGEX } )
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+					uniqueness: { case_sensitive: false }
+	before_save { self.email = email.downcase }
+
+	#has_secure_password
 end
